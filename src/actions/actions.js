dc1620a4c0aa5dbdea14f04e07df469d09495833
Ian Agpawa <agpawaji@gmail.com>
2018-03-07 14:11:59 -0500

    feat: Add export to const in action

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,7 +1,7 @@
 
-const ADD_TODO = 'ADD_TODO';
-const TOGGLE_TODO = 'TOGGLE_TODO';
-const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
+export const ADD_TODO = 'ADD_TODO';
+export const TOGGLE_TODO = 'TOGGLE_TODO';
+export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
 
 
 export const VisibilityFilters = {
