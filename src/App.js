b65d988c4569337dbe55ce68c0e6fa09254c1e41
Ian Agpawa <agpawaji@gmail.com>
2018-03-07 18:52:16 -0500

    refactor: Clean up errors with linking

--- src/App.js
+++ src/App.js
@@ -19,8 +19,8 @@
 // }
 
 import React from 'react';
-import Footer from '.components/Footer';
-import AddTodo from './container/AddTodo';
+import Footer from './components/Footer';
+import AddTodo from './containers/AddTodo';
 import VisibleTodoList from './containers/VisibleTodoList';
 
 const App = () => (
