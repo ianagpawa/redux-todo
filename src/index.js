fbf0e0001d661237b59444c81f84df72f597d488
Ian Agpawa <agpawaji@gmail.com>
2018-03-07 16:25:52 -0500

    refactor: Corrected spelling errors, added redux package

--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
 import React from 'react';
+import { createStore } from 'redux';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import todoApp from './reducers/reducers';
+
+import {
+    addTodo,
+    toggleTodo,
+    setVisibilityFilter,
+    VisibilityFilters
+} from './actions/actions'
+
+let store = createStore(todoApp)
+
+// console.log(store.getState());
+//
+// const unsubscribe = store.subscribe(() =>
+//     console.log(store.getState())
+// )
+//
+//
+// store.dispatch(addTodo('Learn about actions'));
+// store.dispatch(addTodo('Learn about reducers'));
+// store.dispatch(addTodo('Learn about store'));
+// store.dispatch(toggleTodo(0));
+// store.dispatch(toggleTodo(1));
+// store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED));
+//
+// unsubscribe();
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,4 +1,4 @@
-import { combinedReducers } from 'redux';
+import { combineReducers } from 'redux';
 import { ADD_TODO, TOGGLE_TODO, SET_VISIBILITY_FILTER, VisibilityFilters } from '../actions/actions';
 
 
