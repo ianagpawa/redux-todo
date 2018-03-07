0c5f0460dc21fa9ad206d1e217bdc3d3becd8bee
Ian Agpawa <agpawaji@gmail.com>
2018-03-07 15:28:35 -0500

    feat: Add todo to reducer

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,4 +1,4 @@
-import { VisibilityFilters } from '../actions/actions';
+import { ADD_TODO, TOGGLE_TODO, SET_VISIBILITY_FILTER, VisibilityFilters } from '../actions/actions';
 
 const intialState = {
     visibilityFilter: VisibilityFilters.SHOW_ALL,
@@ -11,6 +11,16 @@ function todoApp(state=initialState, action) {
             return Object.assign({}, state, {
                 visibilityFilter: action.filter
             });
+        case ADD_TODO:
+            return Object.assign({}, state, {
+                todos: [
+                    ...state.todos,
+                    {
+                        text: action.text,
+                        completed: false
+                    }
+                ]
+            })
         default:
             return state;
     }
