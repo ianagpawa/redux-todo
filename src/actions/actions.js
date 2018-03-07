ae5b5eb0a36822bb550e465693ea1536b8906307
Ian Agpawa <agpawaji@gmail.com>
2018-03-07 13:57:00 -0500

    feat: Add actions

---
+++ src/actions/actions.js
@@ -0,0 +1,19 @@
+
+const ADD_TODO = 'ADD_TODO';
+
+{
+    type: ADD_TODO,
+    text: 'Build this todo app with redux'
+}
+
+
+{
+    type: TOGGLE_TODO,
+    index: 5
+}
+
+
+{
+    type: SET_VISIBILITY_FILTER,
+    filter: SHOW_COMPLETED
+}
